cffa8bdef50726fdce595a047710de7cd5df0f24
mwf415 <mwf415@126.com>
2019-11-25 19:54:10 +0800

    tijiao

--- src/main/java/com/youyicn/controller/cycle/FileCenter.java
+++ src/main/java/com/youyicn/controller/cycle/FileCenter.java
@@ -40,6 +40,7 @@ import com.youyicn.service.cycle.RoomService;
 /**
  * 主要实现文件的上传和下载功能
  */
+
 @Controller
 public class FileCenter {
 	public Logger log = LoggerFactory.getLogger(File.class);
@@ -47,9 +48,25 @@ public class FileCenter {
 	public RoomService roomService;
 	@Autowired
 	public UserService userService;
-	
+
 	@Autowired
-	public ActivesService activesService;	
+	public ActivesService activesService;
+
+
+
+
+	@RequestMapping("file/downChrome.htm")
+	public void downBrows(HttpServletRequest request ,HttpServletResponse response){
+		String savePath = request.getSession().getServletContext().getRealPath("/");
+		String filePath = savePath	+ "file\\";
+		try {
+			fileDown(response,filePath,"chrome.exe");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 	/**
 	 * 教学查房上传资料
 	 */
@@ -59,28 +76,28 @@ public class FileCenter {
 		session.setAttribute("activesId", id);
 		return "/file/file";
 	}
-	
+
 	//教学查房文件上传
 	@RequestMapping("/file/sumfile.htm")
 	public  void cheRoomfileUp (HttpServletRequest request,HttpServletResponse response ,ModelMap model) throws Exception{
 		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession();
 		Integer activesId = (Integer) session.getAttribute("activesId");
-		
+
 		fileUp(request,activesId);
 		Integer menuOrder = (Integer) session.getAttribute("menuOrder");
 		String li = (String) session.getAttribute("li");
 		String div = (String) session.getAttribute("div");
-		response.sendRedirect("/cycle/activesCont/index.htm?li="+li+"&div="+div+"&type=t&menuOrder="+menuOrder);  
+		response.sendRedirect("/cycle/activesCont/index.htm?li="+li+"&div="+div+"&type=t&menuOrder="+menuOrder);
 	}
 	//教学查房下载列表
 	@RequestMapping("/file/filelist.htm")
-	public String cheRoomDownList(HttpServletRequest request,HttpServletResponse response, ModelMap model,Integer activesId) 
+	public String cheRoomDownList(HttpServletRequest request,HttpServletResponse response, ModelMap model,Integer activesId)
 			throws IOException{
 		//前台获取是乱码，那就根据用户登陆信息查询
 		String savePath = request.getSession().getServletContext().getRealPath("/WEB-INF/");
-		
-		
+
+
 		savePath =savePath+"\\file\\"+activesId+"\\";
 		//用来存储下载列表的
 		if(!"".equals(savePath) && null!= savePath  ){
@@ -92,6 +109,8 @@ public class FileCenter {
         }
         return "/file/chedownlist";
 	}
+
+
 	//教学查房下载
 	@RequestMapping("/file/filedown.htm")
 	public void cheRoomDown(HttpServletRequest request,HttpServletResponse response,ModelMap model) throws UnsupportedEncodingException{
@@ -101,9 +120,9 @@ public class FileCenter {
 		String activesId =file[0];
 		String savePath = request.getSession().getServletContext().getRealPath("/");
 		String filePath = savePath	+ "WEB-INF\\file\\";
-		fileDown(response,filePath,activesId,fileName);
+		fileDown(response,filePath + activesId ,fileName);
 	}
-	
+
 	//模板下载
 	@RequestMapping("/file/mobanfiledown.htm")
 	public void mobanDown(HttpServletRequest request,HttpServletResponse response,ModelMap model) throws UnsupportedEncodingException{
@@ -113,18 +132,18 @@ public class FileCenter {
 		String activesId =file[0];
 		String savePath = request.getSession().getServletContext().getRealPath("/");
 		String filePath = savePath	+ "file\\";
-		fileDown(response,filePath,activesId,fileName);
+		fileDown(response,filePath + activesId,fileName);
 	}
-	
-	
-	
+
+
+
 	/**
 	 * 下载列表  传一个参数  就可以获得相应功能下的文件列表
 	 * 比如如果入科教育，传一个科室的编号就可以显示入科教育的文件
 	 */
 	private List<String> downList(String paramPath) {
 		List<String> downList= new ArrayList<String>();
-        
+
         File file = new File(paramPath);
         String list[] = file.list();
         if (list != null && list.length > 0){
@@ -135,8 +154,8 @@ public class FileCenter {
         }
 		return downList;
 	}
-	
-	
+
+
 	/**
 	 *  文件上传功能
 	 */
@@ -159,8 +178,8 @@ public class FileCenter {
 			ServletFileUpload upload = new ServletFileUpload(factory);
 			upload.setHeaderEncoding("UTF-8");
 			try {
-				MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;  
-	            List<MultipartFile> fileList = multipartRequest.getFiles("file");  
+				MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+	            List<MultipartFile> fileList = multipartRequest.getFiles("file");
 				for (MultipartFile item : fileList) {
 					String filename = item.getOriginalFilename();
 					System.out.println(filename);
@@ -177,7 +196,7 @@ public class FileCenter {
 					}
 					in.close();
 					out.close();
-					
+
 					Actives ac = activesService.getById(activesId);
 					Integer fileNum ;
 					if(null ==ac.getFileNum()){
@@ -192,16 +211,18 @@ public class FileCenter {
 				e.printStackTrace();
 				System.out.println(e);
 			}
-			
-		} 
+
+		}
 	}
+
 	// 文件下载功能；
-	private static void fileDown(HttpServletResponse response, String filePath,String randomNum,String fileName) throws UnsupportedEncodingException {
-		
+	private static void fileDown(HttpServletResponse response, String filePath,String fileName) throws UnsupportedEncodingException {
+
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("multipart/form-data");
 		response.setHeader("Content-Disposition", "attachment;fileName="+ URLEncoder.encode(fileName, "UTF-8"));
-		String savePath = filePath + randomNum+"\\"+fileName;
+		String savePath = filePath +"\\"+fileName;
+
 		try {
 			InputStream inputStream = new FileInputStream(new File(savePath));
 			OutputStream os = response.getOutputStream();
@@ -220,7 +241,7 @@ public class FileCenter {
 		}
 	}
 
-	
+
 
 	/**
 	 * 获取老师名字
@@ -234,5 +255,5 @@ public class FileCenter {
 		}
 		return teacher;
 	}
-	
+
 }
